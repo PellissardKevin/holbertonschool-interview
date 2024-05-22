06dcc480a0e89e5ff5618a659f67a68d58b1a493
Kevin.P <kevin.pellissard@gmail.com>
2024-05-22 12:57:18 +0200

    Task 0 for Algorithm exercicse

---
+++ heap_insert/0-binary_tree_node.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	heap_t *new_node = (heap_t *)malloc(sizeof(heap_t));
+	if (new_node == NULL)
+	{
+		return NULL; // Memory allocation failed
+	}
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return new_node;
+}
