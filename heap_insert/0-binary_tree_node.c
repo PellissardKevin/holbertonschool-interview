8285801b82e59ad98a147e917d09726ea0088f1d
Kevin.P <kevin.pellissard@gmail.com>
2024-05-22 13:33:00 +0200

    betty style

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -2,12 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * binary_tree_node - Entry point
+ *
+ * @parent: Pointer to the parent
+ * @value: value of the node
+ *
+ * Return: Always 0 (Success)
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	heap_t *new_node = (heap_t *)malloc(sizeof(heap_t));
 	if (new_node == NULL)
 	{
-		return NULL; // Memory allocation failed
+		return NULL;
 	}
 
 	new_node->n = value;
