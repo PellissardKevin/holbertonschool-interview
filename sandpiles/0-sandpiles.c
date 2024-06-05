cca74809a3d797070bcec8782c61ba549b706bc3
Kevin.P <kevin.pellissard@gmail.com>
2024-06-05 16:20:51 +0200

    Betty coding style (40lines i hate it...)

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -4,22 +4,78 @@
 /**
  * print_grid - Print 3x3 grid
  * @grid: 3x3 grid
- *
  */
 void print_grid(int grid[3][3])
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            if (j)
-                printf(" ");
-            printf("%d", grid[i][j]);
-        }
-        printf("\n");
-    }
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+/**
+ * is_unstable - Checks if the grid is unstable
+ * @grid: 3x3 grid
+ * Return: 1 if unstable, 0 otherwise
+ */
+int is_unstable(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+				return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * topple - Topples an unstable sandpile
+ * @grid: 3x3 grid
+ */
+void topple(int grid[3][3])
+{
+	int i, j;
+	int temp_grid[3][3] = {0};
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			{
+				temp_grid[i][j] -= 4;
+				if (i > 0)
+					temp_grid[i - 1][j]++;
+				if (i < 2)
+					temp_grid[i + 1][j]++;
+				if (j > 0)
+					temp_grid[i][j - 1]++;
+				if (j < 2)
+					temp_grid[i][j + 1]++;
+			}
+		}
+	}
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid[i][j] += temp_grid[i][j];
+		}
+	}
 }
 
 /**
@@ -29,82 +85,22 @@ void print_grid(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-    int i, j;
-    int unstable;
-    int temp_grid[3][3];
+	int i, j;
 
-    /* Add grid2 to grid1 */
-    for (i = 0; i < 3; i++)
-    {
-        for (j = 0; j < 3; j++)
-        {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+	/* Add grid2 to grid1 */
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
 
-    /* Stabilize the resulting grid */
-    while (1)
-    {
-        unstable = 0;
-
-        /* Check for instability */
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (grid1[i][j] > 3)
-                {
-                    unstable = 1;
-                    break;
-                }
-            }
-            if (unstable)
-                break;
-        }
-
-        if (!unstable)
-            break;
-
-        /* Print the unstable grid */
-        printf("=\n");
-        print_grid(grid1);
-
-        /* Initialize temp_grid */
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                temp_grid[i][j] = 0;
-            }
-        }
-
-        /* Topple the grid */
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                if (grid1[i][j] > 3)
-                {
-                    temp_grid[i][j] -= 4;
-                    if (i > 0)
-                        temp_grid[i - 1][j]++;
-                    if (i < 2)
-                        temp_grid[i + 1][j]++;
-                    if (j > 0)
-                        temp_grid[i][j - 1]++;
-                    if (j < 2)
-                        temp_grid[i][j + 1]++;
-                }
-            }
-        }
-
-        /* Apply the changes */
-        for (i = 0; i < 3; i++)
-        {
-            for (j = 0; j < 3; j++)
-            {
-                grid1[i][j] += temp_grid[i][j];
-            }
-        }
-    }
+	/* Stabilize the resulting grid */
+	while (is_unstable(grid1))
+	{
+		printf("=\n");
+		print_grid(grid1);
+		topple(grid1);
+	}
 }
