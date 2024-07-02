98cd977293173cfb57e45441099bdae8aaaa05eb
Kevin.P <kevin.pellissard@gmail.com>
2024-07-02 11:51:07 +0200

    Add algorithm for find if integer is a palindrome or not.

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,21 @@
+#include "palindrome.h"
+
+int is_palindrome(unsigned long n)
+{
+	unsigned long reversed = 0, original = n, remainder;
+
+	if (n < 10)
+		return (1);
+
+	while (n != 0)
+	{
+		remainder = n % 10;
+		reversed = reversed * 10 + remainder;
+		n /= 10;
+	}
+
+	if (original == reversed)
+		return (1);
+	else
+		return (0);
+}
