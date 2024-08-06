5373bbf24cf17afa8d325f872923b6b321098437
Kevin.P <kevin.pellissard@gmail.com>
2024-08-06 10:58:17 +0200

    Exercice algorithm sorted array to avl, with mid method.

---
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * create_node - Creates a new AVL tree node
+ * @value: Value to be stored in the node
+ * @parent: Parent of the new node
+ * Return: Pointer to the created node, or NULL on failure
+ */
+avl_t *create_node(int value, avl_t *parent)
+{
+    avl_t *node = malloc(sizeof(avl_t));
+    if (!node)
+        return (NULL);
+    node->n = value;
+    node->parent = parent;
+    node->left = NULL;
+    node->right = NULL;
+    return (node);
+}
+
+/**
+ * build_avl_tree - Recursively builds the AVL tree from sorted array
+ * @array: Pointer to the first element of the array
+ * @start: Starting index of the subarray
+ * @end: Ending index of the subarray
+ * @parent: Pointer to the parent node
+ * Return: Pointer to the root node of the AVL tree, or NULL on failure
+ */
+avl_t *build_avl_tree(int *array, int start, int end, avl_t *parent)
+{
+    if (start > end)
+        return (NULL);
+
+    int mid = (start + end) / 2;
+    avl_t *root = create_node(array[mid], parent);
+    if (!root)
+        return (NULL);
+
+    root->left = build_avl_tree(array, start, mid - 1, root);
+    root->right = build_avl_tree(array, mid + 1, end, root);
+
+    return (root);
+}
+
+/**
+ * sorted_array_to_avl - Builds an AVL tree from a sorted array
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * Return: Pointer to the root node of the created AVL tree, or NULL on failure
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+    if (!array || size == 0)
+        return (NULL);
+
+    return (build_avl_tree(array, 0, size - 1, NULL));
+}
---
+++ sorted_array_to_avl/binary_trees.h
@@ -0,0 +1,44 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct binary_tree_s
+{
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *tree);
+
+/**
+ * struct avl_s - AVL Tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
+typedef struct avl_s
+{
+    int n;
+    struct avl_s *parent;
+    struct avl_s *left;
+    struct avl_s *right;
+} avl_t;
+
+avl_t *sorted_array_to_avl(int *array, size_t size);
+
+#endif /* BINARY_TREES_H */
