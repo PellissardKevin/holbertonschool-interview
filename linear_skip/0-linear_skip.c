de03127a86dad711c2107f0b34c683f7f5b34570
Kevin.P <kevin.pellissard@gmail.com>
2024-08-14 16:31:06 +0200

    Changement the max size of the list and expression in the while loop

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -1,10 +1,8 @@
 #include "search.h"
-#include <math.h>
-#include <stdint.h>
 
 /**
- * linear_skip - Searches for a value in a sorted skip
- * list using the express lane
+ * linear_skip - Searches for a value in a sorted skip list
+ * using the express lane
  * @head: Pointer to the head of the skip list
  * @value: Value to search for
  *
@@ -21,29 +19,31 @@ skiplist_t *linear_skip(skiplist_t *head, int value)
 	/* Traverse the express lane to find the block where the value might be */
 	while (node->express && node->express->n < value)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", node->express->index,
-			   node->express->n);
+		printf("Value checked at index [%lu] = [%d]\n",
+		node->express->index,
+		node->express->n);
 		node = node->express;
 	}
 
-	/* At this point, node is at the beginning of the block to search in */
-	express_node = node;
 	/* Find the last node in the list */
 	last_node = node;
 	while (last_node->next)
 		last_node = last_node->next;
 
+	/* At this point, node is at the beginning of the block to search in */
+	express_node = node;
 	printf("Value found between indexes [%lu] and [%lu]\n",
 		   express_node->index,
-		   express_node->express ? express_node->express->index : last_node->index);
+		   express_node->express ?
+		   express_node->express->index : last_node->index);
 
 	/* Perform linear search in the block from express_node to the end */
 	while (express_node && express_node->index <=
-							   (express_node->express ?
-							   express_node->express->index : last_node->index))
+	(express_node->express ? express_node->express->index : last_node->index))
 	{
 		printf("Value checked at index [%lu] = [%d]\n",
-			   express_node->index, express_node->n);
+		express_node->index,
+		express_node->n);
 		if (express_node->n == value)
 			return (express_node);
 		express_node = express_node->next;
