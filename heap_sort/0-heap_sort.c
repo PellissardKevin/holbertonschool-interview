aed799eeef476c28104598f20c23a2c73bf752bf
Kevin.P <kevin.pellissard@gmail.com>
2024-09-02 11:17:58 +0200

    print every step of the sorted algorithm

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -1,6 +1,49 @@
 #include "sort.h"
 
 /**
+ * siftDown - maintains the heap property by sifting down
+ * an element at a given index
+ *
+ * @array: the array representing the heap
+ * @index: the index of the element to sift down
+ * @size: the size of the heap (not the full array size)
+ */
+void siftDown(int *array, int index, size_t size)
+{
+    int largest = index;
+    size_t left = 2 * index + 1;
+    size_t right = 2 * index + 2;
+    int temp;
+
+    // If the left child is larger than the root
+    if (left < size && array[left] > array[largest])
+    {
+        largest = (int)left;
+    }
+
+    // If the right child is larger than the largest so far
+    if (right < size && array[right] > array[largest])
+    {
+        largest = (int)right;
+    }
+
+    // If largest is not the root
+    if (largest != index)
+    {
+        // Swap
+        temp = array[index];
+        array[index] = array[largest];
+        array[largest] = temp;
+
+        // Print the array after each swap
+        print_array(array, size);
+
+        // Recursively sift down the affected subtree
+        siftDown(array, largest, size);
+    }
+}
+
+/**
  * heap_sort - sorts an array of integers in ascending
  * order using the Heap sort algorithm
  *
@@ -9,45 +52,26 @@
  */
 void heap_sort(int *array, size_t size)
 {
-	int temp;
+    int temp;
 
-	for (int i = (size / 2) - 1; i >= 0; i--)
-	{
-		print_array(array, size);
-		siftDown(array, i, size - 1);
-	}
+    // Build max heap
+    for (int i = (int)(size / 2) - 1; i >= 0; i--)
+    {
+        siftDown(array, i, size);
+    }
 
-	for (int i = size - 1; i >= 1; i--)
-	{
-		temp = array[i];
-		array[i] = array[0];
-		array[0] = temp;
+    // Extract elements from the heap one by one
+    for (int i = (int)(size - 1); i > 0; i--)
+    {
+        // Move current root (max) to the end
+        temp = array[0];
+        array[0] = array[i];
+        array[i] = temp;
 
-		print_array(array, size);
-		siftDown(array, 0, i - 1);
-	}
-}
+        // Print the array after each swap
+        print_array(array, size);
 
-void siftDown(int *array, int index, size_t size)
-{
-	int k = index, temp;
-	size_t j = 2 * k + 1;
-
-	while (j <= size)
-	{
-		if (j < size && (array[j] < array[j + 1]))
-			j++;
-
-		if (array[k] < array[j])
-		{
-			temp = array[k];
-			array[k] = array[j];
-			array[j] = temp;
-
-			k = j;
-			j = 2 * k + 1;
-		}
-		else
-			break;
-	}
+        // Call siftDown on the reduced heap
+        siftDown(array, 0, i);
+    }
 }
