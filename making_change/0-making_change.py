da0255c39d91314d571e814a2d21042e51d0a45e
Kevin.P <kevin.pellissard@gmail.com>
2024-10-21 10:26:15 +0200

    PEP8 validation

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Algorithm exercices"""
 
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
@@ -12,7 +13,8 @@ def makeChange(coins, total):
     # Loop over each coin in the list
     for coin in coins:
         for amount in range(coin, total + 1):
-            # Update the dp table with the minimum coins needed for the current amount
+            # Update the dp amount with the minimum coins
+            # needed for the current amount
             dp[amount] = min(dp[amount], dp[amount - coin] + 1)
 
     # If dp[total] is still infinity, return -1 (total cannot be met)
